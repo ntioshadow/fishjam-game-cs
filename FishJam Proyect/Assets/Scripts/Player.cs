b13065d8e1dee3116236c2b3a00766dbf370fd84
Jose Carlos Carrera Carrion <vorimentyt@gmail.com>
2024-04-02 11:05:58 -0600

    Added Functionalllity of Interaction
    You can now interact with a object named Clear Counter

--- FishJam Proyect/Assets/Scripts/Player.cs
+++ FishJam Proyect/Assets/Scripts/Player.cs
@@ -5,15 +5,78 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private GameInputs gameInputs;
+    [SerializeField] private LayerMask interactableLayerMask;
+
+    private Vector3 lastInteractionPosition;
+
+    private void Start(){
+        gameInputs.OnInteractionPressed += GameInputs_OnInteractionPressed;
+    }
+
+    private void GameInputs_OnInteractionPressed(object sender, System.EventArgs e)
+    {
+        
+    }    
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    private void HandleInteraction(){
+        Vector2 inputVector = gameInputs.GetMovementVectorNormalized();
+
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if(movement != Vector3.zero){
+            lastInteractionPosition = movement;
+        }
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractionPosition, out RaycastHit raycastHit, interactDistance,interactableLayerMask)){
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
+                clearCounter.Interact();
+            } 
+        }
+        
+    }
+
+    private void HandleMovement(){
+        
         Vector2 inputVector = gameInputs.GetMovementVectorNormalized();
        
         Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += movement * speed * Time.deltaTime;
 
-        transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * 10.0f);
+        float moveDistance = speed * Time.deltaTime;
+        float playerRadius= .7f;
+        float playerHeight = 1.8f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position +  Vector3.up * playerHeight,playerRadius,movement,moveDistance);
+
+        if(!canMove){
+            Vector3 moveDirX = new Vector3(movement.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position +  Vector3.up * playerHeight,playerRadius,moveDirX,moveDistance);
+
+            if(canMove){
+                movement = moveDirX;
+            } 
+            
+            else {
+                Vector3 moveDirZ = new Vector3(0, 0, movement.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position +  Vector3.up * playerHeight,playerRadius,moveDirZ,moveDistance);
+                if(canMove){
+                    movement = moveDirZ;
+                } 
+                else {
+                }
+            
+            }
+        }
+        if(canMove){
+            transform.position += movement * moveDistance;   
+        }
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movement, Time.deltaTime * rotationSpeed);
     }
 }
 
